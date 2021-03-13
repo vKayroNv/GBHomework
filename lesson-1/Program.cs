36cac6bf8706cfdfaad9168088eaf2033ee626d6
Кирилл <kayron.eldrag@gmail.com>
2021-03-13 13:10:06 +0300

    Initial commit

---
+++ lesson-1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите свое имя: ");
+            string name = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"Привет, {name}, сегодня {DateTime.Now.ToLongDateString()}");
+            Console.ReadLine();
+        }
+    }
+}
