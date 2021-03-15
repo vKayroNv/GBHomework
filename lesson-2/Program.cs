c5b9df920d5463f6faf50c2e8acda60fac66c5ed
Кирилл <kayron.eldrag@gmail.com>
2021-03-15 21:39:51 +0300

    lesson 2

---
+++ lesson-2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lesson_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сообщение:");
+            string message = Console.ReadLine();
+            Console.WriteLine("Перевернутое сообщение:");
+            for (int i = message.Length - 1; i >= 0; i--)
+                Console.Write(message[i]);
+            Console.ReadLine();
+        }
+    }
+}
